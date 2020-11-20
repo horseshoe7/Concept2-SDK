b8b9174acf67f8b85512db70fb979bf0b1c5213e
oconnorfreelance <oconnorfreelance@gmail.com>
2020-11-20 09:17:58 +0100

    changed what gets shown in the example.

--- Example-iOS/Concept2-SDK/PerformanceMonitorViewController.swift
+++ Example-iOS/Concept2-SDK/PerformanceMonitorViewController.swift
@@ -10,62 +10,76 @@ import UIKit
 import Concept2_SDK
 
 class PerformanceMonitorViewController: UIViewController {
-  var performanceMonitor:PerformanceMonitor?
-  
-  var nameDisposable:Disposable?
-  @IBOutlet var nameLabel:UILabel!
-  var beatsPerMinuteDisposable:Disposable?
-  @IBOutlet var beatsPerMinuteLabel:UILabel!
-  var wattsDisposable:Disposable?
-  @IBOutlet var wattsLabel:UILabel!
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    attachObservers()
-    updateUI()
-  }
-  
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    detachObservers()
-  }
-  
-  @IBAction func dismissAction(_ sender:AnyObject?) {
-    self.dismiss(animated: true) { () -> Void in
+    var performanceMonitor:PerformanceMonitor?
+    
+    var nameDisposable:Disposable?
+    @IBOutlet var nameLabel:UILabel!
+    
+    var beatsPerMinuteDisposable:Disposable?
+    //@IBOutlet var beatsPerMinuteLabel:UILabel!
+    
+    var wattsDisposable:Disposable?
+    @IBOutlet var wattsLabel:UILabel!
+    
+    var speedDisposable: Disposable?
+    @IBOutlet var speedLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        attachObservers()
+        updateUI()
     }
-  }
-  
-  // MARK: View Updates
-  fileprivate func updateUI() {
-    nameLabel.text = performanceMonitor?.peripheralName ?? "Unknown"
-  }
-  
-  // MARK: Notifications
-  fileprivate func attachObservers() {
-    detachObservers()
     
-    beatsPerMinuteDisposable = performanceMonitor?.heartRate.attach({
-      [weak self] (strokeRate:C2HeartRate) -> Void in
-      if let weakSelf = self {
-        DispatchQueue.main.async {
-          weakSelf.beatsPerMinuteLabel.text = "\(strokeRate)"
-        }
-      }
-    })
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        detachObservers()
+    }
     
-    wattsDisposable = performanceMonitor?.strokePower.attach({
-      [weak self] (distance:C2Power) -> Void in
-      if let weakSelf = self {
-        DispatchQueue.main.async {
-          weakSelf.wattsLabel.text = "\(distance)"
+    @IBAction func dismissAction(_ sender:AnyObject?) {
+        self.dismiss(animated: true) { () -> Void in
         }
-      }
-    })
-  }
-  
-  fileprivate func detachObservers() {
-    nameDisposable?.dispose()
-    beatsPerMinuteDisposable?.dispose()
-    wattsDisposable?.dispose()
-  }
+    }
+    
+    // MARK: View Updates
+    fileprivate func updateUI() {
+        nameLabel.text = performanceMonitor?.peripheralName ?? "Unknown"
+    }
+    
+    // MARK: Notifications
+    fileprivate func attachObservers() {
+        detachObservers()
+        
+//        beatsPerMinuteDisposable = performanceMonitor?.heartRate.attach({
+//            [weak self] (strokeRate:C2HeartRate) -> Void in
+//            if let weakSelf = self {
+//                DispatchQueue.main.async {
+//                    weakSelf.beatsPerMinuteLabel.text = "\(strokeRate)"
+//                }
+//            }
+//        })
+        
+        wattsDisposable = performanceMonitor?.strokePower.attach({
+            [weak self] (distance:C2Power) -> Void in
+            if let weakSelf = self {
+                DispatchQueue.main.async {
+                    weakSelf.wattsLabel.text = "\(distance)"
+                }
+            }
+        })
+        
+        speedDisposable = performanceMonitor?.speed.attach({ [weak self] (speed: C2Speed) in
+            guard let `self` = self else { return }
+            
+            DispatchQueue.main.async {
+                self.speedLabel.text = "\(String(format: "%.1f", speed))"
+            }
+        })
+    }
+    
+    fileprivate func detachObservers() {
+        nameDisposable?.dispose()
+        beatsPerMinuteDisposable?.dispose()
+        wattsDisposable?.dispose()
+    }
 }
